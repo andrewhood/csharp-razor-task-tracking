1c54884325547e8a1d4f43138d7d827b19ef7c12
Andrew Hood <andrew@andrewhood.ca>
2020-04-15 13:20:18 -0400

    Adding Validation to the Create Form

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -23,6 +23,11 @@ namespace Tasker.Pages
 		public IActionResult OnPost()
 		{
 
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
+
 			_context.Tasks.Add(NewTask);
 			_context.SaveChanges();
 
